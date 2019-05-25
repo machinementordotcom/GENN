24361589fa2137658e3bb0457283cbadcaa651d2
proselotis <gusmoir@me.com>
2019-05-25 11:53:31 -0500

    Final changes to the game and FSM

--- FSMPlayers/HumanPlayer.py
+++ FSMPlayers/HumanPlayer.py
@@ -34,47 +34,4 @@ class HumanPlayer(arcade.Sprite):
         knife.angle = self.angle-180
         self.knife_num += 1 # prevents multiple knifes from being created
         self.knife_list.append(knife)
-    def on_key_press(self, key, modifiers):
-        """Called whenever a key is pressed. """
-        # spatial
-        if key == arcade.key.W:
-            self.player_sprite.change_y = MOVEMENT_SPEED
-        elif key == arcade.key.S:
-            self.player_sprite.change_y = -MOVEMENT_SPEED
-        elif key == arcade.key.A:
-            self.player_sprite.change_x = -MOVEMENT_SPEED
-        elif key == arcade.key.D:
-            self.player_sprite.change_x = MOVEMENT_SPEED
-        # Rotational
-        elif key == arcade.key.LEFT:
-            self.player_sprite.change_angle = ANGLE_SPEED
-        elif key == arcade.key.RIGHT:
-            self.player_sprite.change_angle = -ANGLE_SPEED
-                
-        # Arrow
-        elif key == arcade.key.SPACE:
-            self.shootarrow()
-        # Fireball
-        elif key == arcade.key.E:
-            self.throwfire()
-            # Knife
-        elif key == arcade.key.R and self.player_sprite.knife_num == 0:
-            self.shortattack()
-        elif key == arcade.key.ESCAPE:
-            sys.exit()
-
-        # Calls shield (will simplify and replace with arc that is always in front of player later) can be used 3 times (can be modified if need to)
-        if self.player_sprite.shield <= 3:
-            if key == arcade.key.Q:
-                self.equipshield()
-                
-    def on_key_release(self, key, modifiers):
-        """Called when the user releases a key. """
-
-        if key == arcade.key.W or key == arcade.key.S:
-            self.player_sprite.change_y = 0
-        elif key == arcade.key.A or key == arcade.key.D:
-            self.player_sprite.change_x = 0
-        elif key == arcade.key.LEFT or key == arcade.key.RIGHT:
-            self.player_sprite.change_angle = 0
     
--- main.py
+++ main.py
@@ -8,9 +8,13 @@ def main(args):
     """ Main method """
     simultaneous_games = int(input('Enter the amount of games to be played: '))
     simulation_player_1 = input("What type of simulation do you want for player 1(fsm)?")
-    if simulation_player_1 == "fsm":
+    if simulation_player_1 == "freeplay":
+        player_1_type = "human"
+    elif simulation_player_1 == "fsm":
             player_1_type = input("What type of player is player 1 (short, mid, or range)?")
     simulation_player_2 = input("What type of simulation do you want for player 2(fsm)?")
+    if simulation_player_2 == "freeplay":
+        player_2_type = "human"
     if simulation_player_2 == "fsm":
         player_2_type = input("What type of player is player 2 (short, mid, or range)?")
     window = Game(SCREEN_WIDTH,SCREEN_HEIGHT,SCREEN_TITLE,simultaneous_games,player_1_type,player_2_type)

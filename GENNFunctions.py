95d7641d7d40fdc6a5fa40ee482b010d3be7c009
proselotis <gusmoir@me.com>
2019-10-17 13:49:13 -0500

    Revert "Revert "adding trained weights for the general GENN""
    This reverts commit 8a68b5d7dab9349b8442617a63930ab54807f0f3.

--- GENNFunctions.py
+++ GENNFunctions.py
@@ -2,6 +2,7 @@ import random
 import numpy as np
 from util.constants import * 
 import csv
+import ast
 
 def createNets(conCurrentGame):
     maxlayers = 100
@@ -13,23 +14,19 @@ def createNets(conCurrentGame):
         layers = []
         totalLayers = random.randint(2, maxlayers)
         totalNodes = np.random.randint(1,maxNodes,size=(1,totalLayers)).tolist()[0]
-        # print(totalNodes)
         # Create every layer
         for j in range(totalLayers):
             if j == 0:
                 nodeWeights = np.random.rand(1,inputsNum,totalNodes[0]).tolist()[0]
             else:
                 nodeWeights = np.random.rand(1,totalNodes[j-1],totalNodes[j]).tolist()[0]
-            # print("total amount of weights",len(nodeWeights))  
             layers.append(Layer(nodeWeights))
         layers.append(Layer(np.random.rand(1,totalNodes[len(totalNodes)-1],1).tolist()[0]))
         layers.append(Layer(np.random.rand(1,totalNodes[len(totalNodes)-1],1).tolist()[0]))
         layers.append(Layer(np.random.rand(1,totalNodes[len(totalNodes)-1],1).tolist()[0]))
         layers.append(Layer(np.random.rand(1,totalNodes[len(totalNodes)-1],1).tolist()[0]))
         layers.append(Layer(np.random.rand(1,totalNodes[len(totalNodes)-1],1).tolist()[0]))
-        # print("Total amount of layers",len(layers))
         nets.append(Network(layers))
-    # print("Total amount of nets",len(nets))
     return nets
 
 def createNet(specificLayers = None,specificNodes = None):
@@ -48,7 +45,6 @@ def createNet(specificLayers = None,specificNodes = None):
             nodeWeights = np.random.rand(1,inputsNum,totalNodes[0]).tolist()[0]
         else:
             nodeWeights = np.random.rand(1,totalNodes[j-1],totalNodes[j]).tolist()[0]
-        # print("total amount of weights",len(nodeWeights))  
         layers.append(Layer(nodeWeights))
     layers.append(Layer(np.random.rand(1,totalNodes[len(totalNodes)-1],1).tolist()[0]))
     layers.append(Layer(np.random.rand(1,totalNodes[len(totalNodes)-1],1).tolist()[0]))
@@ -112,57 +108,17 @@ def writeNetworks(nets):
         with open("Genn/weights" + str(i) + ".csv",'w') as myfile:
             wr = csv.writer(myfile, quoting = csv.QUOTE_ALL) 
             for j in range(len(nets[i].layers)):
-                # for k in range(len(nets[i].layers[j].weights)):
                 wr.writerow(nets[i].layers[j].weights)
-                #     wr.write(",")
-                # wr.writerow("\n")
 
-# def readNetworks(nets,path = None):
-
-#     if path == None:
-#         for i in range(len())
-#     def readWeights(self,path = None):
-#         tempWeights = [[],[]] 
-#         if path == None:
-#             for i in range(self.conGames):
-#                 with open('DynamicController/weightsDynamicController' + self.id + "-" + str(self.conCurrentGameId) + '.csv') as csvfile:
-#                     reader = csv.reader(csvfile)
-#                     weightType = 0
-#                     for row in reader:
-#                         tempWeights[weightType].append([float(i) for i in row])
-#                         weightType +=1
-#             self.weights[0] = np.average(np.array(tempWeights[0]),axis = 0).tolist()
-#             self.weights[1] = np.average(np.array(tempWeights[1]),axis = 0).tolist()
-#         else:
-#             with open(path) as csvfile:
-#                 reader = csv.reader(csvfile)
-#                 weightType = 0
-#                 for row in reader:
-#                     self.weights[weightType] = [float(i) for i in row]
-#                     weightType +=1
-
-
-# def createChildNets(parents,number):
-#     newNets = []
-#     for i in range(number):
-#         parent1 = parents[random.randint(0, len(parents)-1)]
-#         parent2 = parents[random.randint(0, len(parents)-1)]
-#         layers = []
-#         for i in range(max(len(parent1.layers),len(parent2.layers))):
-#             if i < len(parent1.layers) and i < len(parent2.layers):
-#                 if np.random.random_sample() > .5:
-#                     layers.append(parent1.layers[i])
-#                 else:
-#                     layers.append(parent2.layers[i])
-#             elif i < len(parent1.layers):
-#                 if np.random.random_sample() > .5: layers.append(parent1.layers[i])
-#                 else: break
-#             elif i < len(parent2.layers):
-#                 if np.random.random_sample() > .5: layers.append(parent2.layers[i])
-#                 else: break 
-#         newNets.append(Network(layers))
-#     return newNets
-
-
+def readNets(nets):
+    with open("Genn/masterWeights/weights.csv") as csvfile:
+        reader = csv.reader(csvfile)
+        layers = []
+        for row in reader:
+            temp = []
+            for j in row:
+                temp.append(ast.literal_eval(j))
+            layers.append(Layer(temp))
+        return [Network(layers)] * nets  
 
 
--- main.py
+++ main.py
@@ -26,49 +26,56 @@ def main(args):
     """ Main method """
 
     graphics = 'no'
+    train = 'no'
     evolutions = False
     spacer()
 
-    # conCurrentGame = 100
-    # iterations = 999 
-    # simulation_player_1 = 'genn'
-    # simulation_player_2 = 'fsm'
-    # player_2_type = 'mid'
-    # graphics = 'no'
-    # player_1_type = 'genn'
+    if train == 'yes':
+        conCurrentGame = 100
+        iterations = 999 
+        simulation_player_1 = 'genn'
+        simulation_player_2 = 'fsm'
+        player_2_type = 'mid'
+        graphics = 'no'
+        player_1_type = 'genn'
+    else:
+        conCurrentGame = get_int_choice('How many games would you like played at the same time (Recommended amount based on computer cores '+str(multiprocessing.cpu_count())+"):",1,1000)
+        iterations = get_int_choice('Enter the amount of generations to be played: ',1,5000)
+        simulation_player_1 = get_str_choice("What type of simulation do you want for player 1?",'fsm','freeplay','dc','genn')
+        if simulation_player_1.lower() == "freeplay":
+            player_1_type = "human"
+            graphics = 'yes'
+        elif simulation_player_1.lower() == "fsm":
+            player_1_type = get_str_choice("What type of player is player 1 ?",'short','mid','range','pq')
+        elif simulation_player_1.lower() == "dc":
+            player_1_type = get_str_choice("What type of dynamic controller is player 1 ?",'master','average','random','train')
+        elif simulation_player_1.lower() == "genn":
+            player_1_type = "genn"
+        simulation_player_2 = get_str_choice("What type of simulation do you want for player 2?",'fsm','dc','freeplay','genn')
+        if simulation_player_2 == "freeplay":
+            player_2_type = "human"
+            graphics = 'yes'
+        if simulation_player_2 == "fsm":
+            player_2_type = get_str_choice("What type of player is player 2?",'short','mid','range','pq')
+        elif simulation_player_2.lower() == "dc":
+            player_2_type = get_str_choice("What type of dynamic controller is player 2 ?",'master','average','random')
+        elif simulation_player_2.lower() == "genn":
+            player_2_type = "genn"
+        if graphics == 'no':
+            graphics = get_str_choice('Run Graphically?: ','yes','no')
 
-    conCurrentGame = get_int_choice('How many games would you like played at the same time (Recommended amount based on computer cores '+str(multiprocessing.cpu_count())+"):",1,1000)
-    iterations = get_int_choice('Enter the amount of generations to be played: ',1,5000)
-    simulation_player_1 = get_str_choice("What type of simulation do you want for player 1?",'fsm','freeplay','dc','genn')
-    if simulation_player_1.lower() == "freeplay":
-        player_1_type = "human"
-        graphics = 'yes'
-    elif simulation_player_1.lower() == "fsm":
-        player_1_type = get_str_choice("What type of player is player 1 ?",'short','mid','range','pq')
-    elif simulation_player_1.lower() == "dc":
-        player_1_type = get_str_choice("What type of dynamic controller is player 1 ?",'master','average','random','train')
-    elif simulation_player_1.lower() == "genn":
-        player_1_type = "genn"
-    simulation_player_2 = get_str_choice("What type of simulation do you want for player 2?",'fsm','dc','freeplay','genn')
-    if simulation_player_2 == "freeplay":
-        player_2_type = "human"
-        graphics = 'yes'
-    if simulation_player_2 == "fsm":
-        player_2_type = get_str_choice("What type of player is player 2?",'short','mid','range','pq')
-    elif simulation_player_2.lower() == "dc":
-        player_2_type = get_str_choice("What type of dynamic controller is player 2 ?",'master','average','random')
-    if graphics == 'no':
-        graphics = get_str_choice('Run Graphically?: ','yes','no')
-
-    if player_1_type == 'genn':
+    if player_1_type == 'genn' and train == 'yes':
         evolutions = True
         player_1_nets = createNets(conCurrentGame)
+    elif player_1_type == 'genn' and train == 'no':
+        player_1_nets = readNets(conCurrentGame)
     else: player_1_nets = None
-    if player_2_type == 'genn':
+    if player_2_type == 'genn' and train == 'yes':
         evolutions = True
         player_2_nets = createNets(conCurrentGame)
+    elif player_2_type == 'genn' and train == 'no':
+        player_2_nets = readNets(conCurrentGame)
     else: player_2_nets = None
-    # count = Counter()
     if graphics == 'yes':
         window = MyGame(SCREEN_WIDTH,SCREEN_HEIGHT,SCREEN_TITLE,iterations,player_1_type,player_2_type)
         window.setup()
@@ -89,7 +96,7 @@ def main(args):
         for game in range(iterations):
             spacer()
             print("Total iterations %d out of %d" % (game + 1, iterations) )
-            if evolutions == True:
+            if evolutions == True and train == 'yes':
                 if player_1_type == 'genn':
                     if game % 9 == 0 and game != 0:
                         print(evolutionHealth)
@@ -107,20 +114,21 @@ def main(args):
                         temp = createChildNets(newNets,conCurrentGame - len(newNets))
                         player_2_nets = newNets + temp
                         player_2_nets = mutateNets(player_2_nets)
-
             p = multiprocessing.Pool(multiprocessing.cpu_count())
                 # map will always return the results in order, if order is not important in the future use pool.imap_unordered()
-            if game % 9 < 3: player_2_type == 'short'
-            elif game % 9 < 6: player_2_type == 'mid'
-            else: player_2_type == 'range'
+            if train == 'yes':
+                if game % 9 < 3: player_2_type == 'short'
+                elif game % 9 < 6: player_2_type == 'mid'
+                else: player_2_type == 'range'
             result = p.map(runOneGame,[ x + [i - 1]  for i,x in enumerate([ x for x in [[SCREEN_WIDTH,SCREEN_HEIGHT,SCREEN_TITLE,1,player_1_type,player_2_type,conCurrentGame,game,player_1_nets,player_2_nets]] *conCurrentGame  ],1) ])
             if game == 0 or game % 3 == 0: evolutionHealth = [float(i) for i in result]
             else: evolutionHealth = list(map(add, [float(i) for i in result], evolutionHealth)) 
             player1Wins += sum(int(i) > 0 for i in [int(i) for i in result]) 
             player2Wins += sum(int(i) < 0 for i in [int(i) for i in result])
-            if game % 9 < 3: shortWins += sum(int(i) < 0 for i in [int(i) for i in result])
-            elif game % 9 < 6: midWins += sum(int(i) < 0 for i in [int(i) for i in result])
-            else: rangeWins += sum(int(i) < 0 for i in [int(i) for i in result])
+            if train == 'yes':
+                if game % 9 < 3: shortWins += sum(int(i) < 0 for i in [int(i) for i in result])
+                elif game % 9 < 6: midWins += sum(int(i) < 0 for i in [int(i) for i in result])
+                else: rangeWins += sum(int(i) < 0 for i in [int(i) for i in result])
             draws += sum(int(i) == 0 for i in [int(i) for i in result])  
             leftOverHealth += sum([float(i) for i in result])
             p.close()
@@ -131,9 +139,10 @@ def main(args):
             writeNetworks(player_2_nets)
         print("player 1 (" + player_1_type + "):",player1Wins)
         print("player 2 (" + player_2_type + "):",player2Wins)
-        print("\t Short Wins: ",shortWins)
-        print("\t mid Wins: ",midWins)
-        print("\t range Wins: ",rangeWins)
+        if train == 'yes':
+            print("\t Short Wins: ",shortWins)
+            print("\t mid Wins: ",midWins)
+            print("\t range Wins: ",rangeWins)
         print("Draws: ",draws)
         print("Average Health Difference: ",round(abs(leftOverHealth) / (conCurrentGame * iterations),4))
         print("Total Time: ",round(time.time() - start,4))

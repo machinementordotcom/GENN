de1b5ded67e85acc5210ee8e982b82f2d370580d
proselotis <gusmoir@me.com>
2020-03-09 08:13:55 -0500

    moving GENN

---
+++ GENN/GENN.py
@@ -0,0 +1,200 @@
+import sys
+import csv 
+import arcade
+import numpy as np
+from util.constants import *
+import math
+import random
+class GENN(arcade.Sprite):
+
+    def shootarrow(self):
+      arrow = Arrow("images/arrow.png",.1)
+      arrow.center_x = self.center_x
+      arrow.center_y = self.center_y
+      arrow.start_x = self.center_x # for tracking 
+      arrow.start_y = self.center_y
+      arrow.angle = self.angle-90
+      arrow.change_x = -ARROW_SPEED*math.sin(math.radians(self.angle))
+      arrow.change_y = ARROW_SPEED*math.cos(math.radians(self.angle))
+      arrow.vel = ARROW_SPEED
+      arrow.box = BOX
+
+      self.arrow_list.append(arrow)
+
+      hit = HitBox("images/fire.png")
+      hit._set_alpha(0)
+      hit._set_height(math.sqrt(SCREEN_WIDTH**2 + SCREEN_HEIGHT**2))
+      hit._set_width(ARROW_IMAGE_HEIGHT)
+      hit.angle = self.angle
+      hit.center_x = self.center_x + -math.sin(math.radians(hit.angle)) * hit.height/2
+      hit.center_y = self.center_y + math.cos(math.radians(hit.angle)) * hit.height/2
+      hit.vel = ARROW_SPEED
+      hit.box = BOX
+      arrow.hit = hit
+      self.hitbox_list.append(hit)
+    def equipshield(self):
+      self.health += PLAYER_HEALTH*.5
+      self.shield +=1
+    def throwfire(self):
+      fireball = Fireball("images/fire.png", .1)
+      fireball.center_x = self.center_x
+      fireball.center_y = self.center_y
+      fireball.start_x = self.center_x # for tracking 
+      fireball.start_y = self.center_y # fireball distance
+      fireball.angle = self.angle-90
+      fireball.change_x = -ARROW_SPEED*math.sin(math.radians(self.angle))
+      fireball.change_y = ARROW_SPEED*math.cos(math.radians(self.angle))
+      fireball.vel = ARROW_SPEED
+      fireball.box = BOX
+      self.fireball_list.append(fireball)
+
+      hit = HitBox("images/fire.png")
+      hit._set_alpha(0)
+      hit._set_height(math.sqrt(SCREEN_WIDTH**2 + SCREEN_HEIGHT**2))
+      hit._set_width(ARROW_IMAGE_HEIGHT)
+      hit.angle = self.angle
+      hit.center_x = self.center_x + -math.sin(math.radians(hit.angle)) * hit.height/2
+      hit.center_y = self.center_y + math.cos(math.radians(hit.angle)) * hit.height/2
+      hit.vel = ARROW_SPEED
+      hit.box = BOX
+
+      fireball.hit = hit
+      self.hitbox_list.append(hit)
+
+    def shortattack(self):
+      knife = Knife("images/knife.png",.1)
+      knife.center_x = self.center_x
+      knife.center_y = self.center_y
+      knife.angle = self.angle-180
+      knife.box = BOX 
+      self.knife_num += 1 # prevents multiple knifes from being created
+      self.knife_list.append(knife)
+
+    def writeWeights(self):
+      with open("GENN/weightsDynamicController" + self.id + "-" + str(self.conCurrentGameId) + ".csv", 'w') as myfile:
+         wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+         for i in range(2):
+            wr.writerow(self.weights[i])
+    def readWeights(self,path = None):
+     tempWeights = [[],[]] 
+     if path == None:
+         for i in range(self.conGames):
+            with open('GENN/weightsDynamicController' + self.id + "-" + str(self.conCurrentGameId) + '.csv') as csvfile:
+                 reader = csv.reader(csvfile)
+                 weightType = 0
+                 for row in reader:
+                     tempWeights[weightType].append([float(i) for i in row])
+                     weightType +=1
+         self.weights[0] = np.average(np.array(tempWeights[0]),axis = 0).tolist()
+         self.weights[1] = np.average(np.array(tempWeights[1]),axis = 0).tolist()
+     else:
+         with open(path) as csvfile:
+            reader = csv.reader(csvfile)
+            weightType = 0
+            for row in reader:
+               self.weights[weightType] = [float(i) for i in row]
+               weightType +=1
+    def update(self):
+      self.curtime += 1
+      if len(self.opponent_hitbox_list) >= 3:
+        opp_proj_1_x = self.opponent_hitbox_list[0].center_x
+        opp_proj_1_y = self.opponent_hitbox_list[0].center_y
+        opp_proj_2_x = self.opponent_hitbox_list[1].center_x
+        opp_proj_2_y = self.opponent_hitbox_list[1].center_y
+        opp_proj_3_x = self.opponent_hitbox_list[2].center_x
+        opp_proj_3_y = self.opponent_hitbox_list[2].center_y
+      elif len(self.opponent_hitbox_list) == 2:
+        opp_proj_1_x = self.opponent_hitbox_list[0].center_x
+        opp_proj_1_y = self.opponent_hitbox_list[0].center_y
+        opp_proj_2_x = self.opponent_hitbox_list[1].center_x
+        opp_proj_2_y = self.opponent_hitbox_list[1].center_y
+        opp_proj_3_x = 0
+        opp_proj_3_y = 0
+      elif len(self.opponent_hitbox_list) == 1:
+        opp_proj_1_x = self.opponent_hitbox_list[0].center_x
+        opp_proj_1_y = self.opponent_hitbox_list[0].center_y
+        opp_proj_2_x = 0
+        opp_proj_2_y = 0
+        opp_proj_3_x = 0
+        opp_proj_3_y = 0
+      else:
+        opp_proj_1_x = 0
+        opp_proj_1_y = 0
+        opp_proj_2_x = 0
+        opp_proj_2_y = 0
+        opp_proj_3_x = 0
+        opp_proj_3_y = 0
+      inputs = [[self.center_x,self.center_y,self.opponent.center_x,self.opponent.center_x,self.health,self.opponent.health,self.total_time,self.shield,self.opponent.shield,self.curtime,len(self.opponent_hitbox_list), opp_proj_1_x, opp_proj_1_y, opp_proj_2_x, opp_proj_2_y, opp_proj_3_x, opp_proj_3_y]]
+      choices = self.model.predict(np.asarray(inputs))
+      self.center_x += MOVEMENT_SPEED * choices[0][0][0]
+      self.center_y += MOVEMENT_SPEED * choices[1][0][0]
+
+      if self.center_y >= SCREEN_HEIGHT -20:
+          self.center_y = SCREEN_HEIGHT -20
+      if self.center_y <= 0 + 20:
+          self.center_y = 0 + 20
+      if self.center_x >= SCREEN_WIDTH -20:
+          self.center_x = SCREEN_WIDTH -20
+      if self.center_x <= 0 + 20:
+          self.center_x = 0 + 20
+
+      x_diff = self.opponent.center_x - self.center_x
+      y_diff = self.opponent.center_y - self.center_y
+      self.d = math.sqrt(x_diff**2 +y_diff**2)
+      self.angle = math.degrees(math.atan2(y_diff,x_diff))-90
+      self.change_x = -math.cos(self.angle)*MOVEMENT_SPEED
+      self.change_y = -math.sin(self.angle)*MOVEMENT_SPEED
+      if self.center_x > self.opponent.center_x and choices[0][0][0] < 0 and self.center_y > self.opponent.center_y and choices[1][0][0] < 0 or  self.center_x < self.opponent.center_x and choices[0][0][0] >0 and self.center_y > self.opponent.center_y and choices[1][0][0] < 0 or  self.center_x < self.opponent.center_x and choices[0][0][0] >0 and self.center_y < self.opponent.center_y and choices[1][0][0] > 0 or  self.center_x > self.opponent.center_x and choices[0][0][0] < 0 and self.center_y < self.opponent.center_y and choices[1][0][0] > 0:
+        self.trends['towardsOpponent'] += 1
+        if self.lastMovement == "Away":
+          self.trends['movementChanges'] +=1 
+          self.currentTrend = 0
+        else:
+          self.currentTrend +=1 
+          if self.currentTrend > self.trends['biggestTrend']:
+            self.trends['biggestTrend'] = self.currentTrend
+        self.lastMovement = "Towards"
+      else:
+        self.trends['awayOpponent'] += 1
+        if self.lastMovement == "Towards":
+          self.trends['movementChanges'] +=1 
+          self.currentTrend = 0
+        else:
+          self.currentTrend +=1 
+          if self.currentTrend > self.trends['biggestTrend']:
+            self.trends['biggestTrend'] = self.currentTrend
+        self.lastMovement = "away"
+      if self.curtime >=30:
+        if choices[2][0][0] == choices[3][0][0] == choices[4][0][0]:
+          attack = random.choices(['short','mid','range'])[0]
+          if attack == 'short': 
+            self.shortattack()
+            self.trends['knife'] = self.trends['knife'] + 1
+          elif attack == 'mid': 
+            self.throwfire()
+            self.trends['fire'] = self.trends['fire'] + 1
+          else: 
+            self.shootarrow()
+            self.trends['arrow'] = self.trends['arrow'] + 1
+        elif choices[2][0][0] > choices[3][0][0] and choices[2][0][0] > choices[4][0][0]:
+          self.shortattack()
+        elif choices[3][0][0] > choices[4][0][0]:
+          self.throwfire()
+        else:
+          self.shootarrow()
+        self.curtime = 0
+      for arrow in self.arrow_list:
+        if arrow.center_x>SCREEN_WIDTH + 10 or arrow.center_y>SCREEN_HEIGHT + 10 or arrow.center_x< -10 or arrow.center_y< -10 :
+            arrow.hit.kill()
+            arrow.kill()
+        for fireball in self.fireball_list:
+            diff_x = fireball.start_x-fireball.center_x
+            diff_y = fireball.start_y-fireball.center_y
+            fireball_dist = math.sqrt(diff_x**2 + diff_y**2)
+            if fireball_dist>200:
+                self.fireball_list.remove(fireball)
+
+
+      if self.health <= PLAYER_HEALTH*.5 and self.shield < 1:
+        self.equipshield()
+
---
+++ GENN/GENNFunctions.py
@@ -0,0 +1,124 @@
+import random 
+import numpy as np
+from util.constants import * 
+import csv
+import ast
+
+def createNets(conCurrentGame):
+    maxlayers = 100
+    maxNodes = 100
+    inputsNum = 17
+    nets = []
+    # Create every network
+    for i in range(conCurrentGame):
+        layers = []
+        totalLayers = random.randint(2, maxlayers)
+        totalNodes = np.random.randint(1,maxNodes,size=(1,totalLayers)).tolist()[0]
+        # Create every layer
+        for j in range(totalLayers):
+            if j == 0:
+                nodeWeights = np.random.rand(1,inputsNum,totalNodes[0]).tolist()[0]
+            else:
+                nodeWeights = np.random.rand(1,totalNodes[j-1],totalNodes[j]).tolist()[0]
+            layers.append(Layer(nodeWeights))
+        layers.append(Layer(np.random.rand(1,totalNodes[len(totalNodes)-1],1).tolist()[0]))
+        layers.append(Layer(np.random.rand(1,totalNodes[len(totalNodes)-1],1).tolist()[0]))
+        layers.append(Layer(np.random.rand(1,totalNodes[len(totalNodes)-1],1).tolist()[0]))
+        layers.append(Layer(np.random.rand(1,totalNodes[len(totalNodes)-1],1).tolist()[0]))
+        layers.append(Layer(np.random.rand(1,totalNodes[len(totalNodes)-1],1).tolist()[0]))
+        nets.append(Network(layers))
+    return nets
+
+def createNet(specificLayers = None,specificNodes = None):
+    maxlayers = 100
+    maxNodes = 100
+    inputsNum = 17
+    # Create every network
+    layers = []
+    if specificLayers == None: totalLayers = random.randint(2, maxlayers)
+    else: totalLayers = specificLayers 
+    if specificNodes == None: totalNodes = np.random.randint(1,maxNodes,size=(1,totalLayers)).tolist()[0]
+    else: totalNodes = specificNodes
+    # Create every layer
+    for j in range(totalLayers):
+        if j == 0:
+            nodeWeights = np.random.rand(1,inputsNum,totalNodes[0]).tolist()[0]
+        else:
+            nodeWeights = np.random.rand(1,totalNodes[j-1],totalNodes[j]).tolist()[0]
+        layers.append(Layer(nodeWeights))
+    layers.append(Layer(np.random.rand(1,totalNodes[len(totalNodes)-1],1).tolist()[0]))
+    layers.append(Layer(np.random.rand(1,totalNodes[len(totalNodes)-1],1).tolist()[0]))
+    layers.append(Layer(np.random.rand(1,totalNodes[len(totalNodes)-1],1).tolist()[0]))
+    layers.append(Layer(np.random.rand(1,totalNodes[len(totalNodes)-1],1).tolist()[0]))
+    layers.append(Layer(np.random.rand(1,totalNodes[len(totalNodes)-1],1).tolist()[0]))
+    return Network(layers)
+
+def createChildNets(parents,number):
+    return createNets(number)
+    newNets = []
+    inputsNum = 17
+    maxNodes = 100
+    for i in range(number):
+        parent1 = parents[random.randint(0, len(parents)-1)]
+        parent2 = parents[random.randint(0, len(parents)-1)]
+        if np.random.random_sample() > .5:totalLayers = len(parent1.layers)
+        else:totalLayers = len(parent2.layers)
+        totalNodes = np.random.randint(1,maxNodes,size=(1,totalLayers)).tolist()[0]
+        layers = []
+        for j in range(totalLayers -1):
+            if j == 0:
+                nodeWeights = np.random.rand(1,inputsNum,totalNodes[0]).tolist()[0]
+            else:
+                nodeWeights = np.random.rand(1,totalNodes[j-1],totalNodes[j]).tolist()[0]
+            layers.append(Layer(nodeWeights))
+        layers.append(Layer(np.random.rand(1,totalNodes[len(totalNodes)-1],1).tolist()[0]))
+        layers.append(Layer(np.random.rand(1,totalNodes[len(totalNodes)-1],1).tolist()[0]))
+        layers.append(Layer(np.random.rand(1,totalNodes[len(totalNodes)-1],1).tolist()[0]))
+        layers.append(Layer(np.random.rand(1,totalNodes[len(totalNodes)-1],1).tolist()[0]))
+        layers.append(Layer(np.random.rand(1,totalNodes[len(totalNodes)-1],1).tolist()[0]))
+        newNets.append(Network(layers))
+    return newNets
+
+def countBits(n):
+  n = (n & 0x5555555555555555) + ((n & 0xAAAAAAAAAAAAAAAA) >> 1)
+  n = (n & 0x3333333333333333) + ((n & 0xCCCCCCCCCCCCCCCC) >> 2)
+  n = (n & 0x0F0F0F0F0F0F0F0F) + ((n & 0xF0F0F0F0F0F0F0F0) >> 4)
+  n = (n & 0x00FF00FF00FF00FF) + ((n & 0xFF00FF00FF00FF00) >> 8)
+  n = (n & 0x0000FFFF0000FFFF) + ((n & 0xFFFF0000FFFF0000) >> 16)
+  n = (n & 0x00000000FFFFFFFF) + ((n & 0xFFFFFFFF00000000) >> 32) # This last & isn't strictly necessary.
+  return n
+def toggleKthBit(n, k): 
+    return (n ^ (1 << (k-1))) 
+
+def mutateNets(nets):
+    newNetsIndex = np.random.randint(0,len(nets),int(len(nets)*.1))
+    for i in newNetsIndex:
+        current = nets[i]
+        currentLayers = len(current.layers)
+        num = countBits(currentLayers)
+        for i in range(num):
+            if random.uniform(0, 1) < (1/num):
+                currentLayers = toggleKthBit(currentLayers,i)
+        nets[i] = createNet(specificLayers = currentLayers)
+    return nets
+
+def writeNetworks(nets):
+
+    for i in range(len(nets)):
+        with open("Genn/weights" + str(i) + ".csv",'w') as myfile:
+            wr = csv.writer(myfile, quoting = csv.QUOTE_ALL) 
+            for j in range(len(nets[i].layers)):
+                wr.writerow(nets[i].layers[j].weights)
+
+def readNets(nets):
+    with open("Genn/masterWeights/weights.csv") as csvfile:
+        reader = csv.reader(csvfile)
+        layers = []
+        for row in reader:
+            temp = []
+            for j in row:
+                temp.append(ast.literal_eval(j))
+            layers.append(Layer(temp))
+        return [Network(layers)] * nets  
+
+
